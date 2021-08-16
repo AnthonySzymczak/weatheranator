0cad7faa993ef23f31e7013139a273ca570bf98f
AnthonySzymczak <doyalajs1@live.com>
2021-08-16 14:37:50 -0400

    fetch worx

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # weatheranator
 
-In this project, you will be able to search for your desired city to plan a 5-day trip too.
+In this project, you will be able to search for your desired city to plan a 5-day trip to.
 
 Once you enter in your desired city, you will be then shown a 5day forecast of the area;
 you shall be presented with the city name, the date, a representation of weather conditions in an icon, the temperature, the humidity, the wind speed, and the UV index noted inside each card.
-\
+
 
 Screenshot of the blank dashboard:
 
@@ -12,3 +12,4 @@ Screenshot of typed in search bar:
 
 
 Here is a link to the deployed appliction:
+https://anthonyszymczak.github.io/weatheranator/
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,30 @@
-let key = "";
-let city = "YOUR CITY"; // My test case was "London"
-let url = "https://api.openweathermap.org/data/2.5/forecast";
+let url = "https://api.openweathermap.org/data/2.5/weather";
+let searchBtn = $("#search");
+// let mainContainer = $("#mainContainer");
 
+let key = "apikey";
+let apikey = "5ff1358d690bc090170fefc4f83ac907";
+
+
+// on click to generate resulats
+
+searchBtn.click(weatherResults)
+const userInput = document.querySelector(".input_text")
+//function to call all functions
+function weatherResults(e){
+    let city = userInput.value;
+    console.log(city)
+    renderLatLon(city);
+};
+
+async function renderLatLon(userInput){
+
+let weatherApi = url + "?q=" + userInput + "&appid=" + apikey;
+let res = await fetch(weatherApi)
+let data = await res.json()
+console.log(data);
+};
+
+// Generate cards
+// Put info in cards
+// stop crying
